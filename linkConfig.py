06a5aa5036bd70ca0411e86ca04168113ee767fb
Matthias Antony <amatthias@ethz.ch>
2025-11-23 13:05:37 +0100

    Added a bunch of config files and reorganised repository structure

--- linkConfig.py
+++ linkConfig.py
@@ -1,23 +1,13 @@
-#!/usr/bin/env python
-
 import getpass
-
 user = getpass.getuser()
 
 class Config(object):
     Linux = {
-        "bash/.bashrc"              : "/home/" + user + "/.bashrc",
-        "bash/.bash_aliases"        : "/home/" + user + "/.bash_aliases",
-        "bash/.bash_prompt"         : "/home/" + user + "/.bash_prompt",
-        "git/.gitconfig"            : "/home/" + user + "/.gitconfig",
-        "vim/.vimrc"                : "/home/" + user + "/.vimrc",
-        "i3/config"                 : "/home/" + user + "/.config/i3/config",
-        # "i3/scripts/sway_bar.sh"  : "/home/" + user + "/.config/sway/scripts/sway_bar.sh",
-        "background/jwst.jpg"       : "/home/" + user + "/Pictures/jwst.jpg",
-        "background/jwst_pic.png"   : "/home/" + user + "/Pictures/jwst_pic.png"
+        '.config/git',
+        '.config/i3blocks',
+        '.config/nvim',
+        '.config/sway',
+        '.config/vim',
+        '.config/zsh',
+        '.zshenv'
     }
-
-    LinuxDirs = [
-        "/home/" + user + "/.config/sway/scripts/",
-        "/home/" + user + "/Pictures/"
-    ]
